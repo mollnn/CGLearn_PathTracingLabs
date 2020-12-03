00a29c342fc3ed243ed3664c93375324ea9bf479
mollnn <1084918073@qq.com>
2020-12-03 11:20:05 +0800

    配置项目目录

--- PathTracingLab1/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main()
-{
-    cout << "Path Tracing Lab 1: A Simple Monochrome Path Tracer." << endl;
-    return 0;
-}
---
+++ PathTracingLab1/src/Main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Path Tracing Lab 1: A Simple Monochrome Monte Carlo Path Tracer." << endl;
+    return 0;
+}
