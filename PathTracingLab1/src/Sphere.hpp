053e67313a6c1e8160bae9f3e821415e5f16d308
mollnn <1084918073@qq.com>
2020-12-04 19:32:07 +0800

    Sphere 完成

--- PathTracingLab1/src/Sphere.hpp
+++ PathTracingLab1/src/Sphere.hpp
@@ -1,19 +1,17 @@
 #ifndef _SPHERE_HPP_
 #define _SPHERE_HPP_
 
-
-
 #include <bits/stdc++.h>
 #include "json/CJsonObject.hpp"
 #include "Vector3D.hpp"
 #include "Material.hpp"
+#include "Ray.hpp"
 
-struct Sphere 
+struct Sphere
 {
     Vector3D center;
     double radius;
     Material material;
-
 
     // () <-
 
@@ -25,8 +23,32 @@ struct Sphere
     // (delta + t*direction) * (delta + t*direction) = radius * radius
     // delta*delta + 2*delta*direction * t + direction*direction * t^2 = radius * radius
     // (direction*direction) * t^2 + 2*delta*direction * t + delta*delta-radius*radius = 0
-    
-};
 
+    double GetIntersectionDistance(const Sphere &sphere, const Ray &ray)
+    {
+        Vector3D direction = ray.direction;
+        Vector3D center_to_origin = ray.origin - sphere.center;
+        double radius = sphere.radius;
+
+        double a = direction.Dot(direction);
+        double b = 2 * center_to_origin.Dot(direction);
+        double c = center_to_origin.Dot(center_to_origin) - radius * radius;
+
+        // (-b +- sqrt(b^2 - 4ac)) / 2
+
+        double delta = sqrt(b * b - 4 * a * c);
+        double t1 = (-b - delta) / 2;
+        double t2 = (-b + delta) / 2;
+
+        if (t1 >= 0)
+        {
+            return t1;
+        }
+        else
+        {
+            return t2;
+        }
+    }
+};
 
 #endif
