2698bc513ba43b26bf4958bcd0f05966e75fc95b
mollnn <1084918073@qq.com>
2020-12-07 11:31:45 +0800

    ColorSpace.hpp 完成

---
+++ PathTracingLab1/src/ColorSpace.hpp
@@ -0,0 +1,19 @@
+#ifndef _COLORSPACE_HPP
+#define _COLORSPACE_HPP
+
+#include <bits/stdc++.h>
+#include "json/CJsonObject.hpp"
+
+double Clamp(double x)
+{
+    x = std::min(x, 1.0);
+    x = std::max(x, 0.0);
+    return x;
+}
+
+double IntensityToBrightness(double x, double gamma = 2.2)
+{
+    return pow(x, 1.0 / gamma);
+}
+
+#endif
--- PathTracingLab1/src/moduleTest.cpp
+++ PathTracingLab1/src/moduleTest.cpp
@@ -8,15 +8,15 @@
 
 void module_test_scene_test()
 {
-    Scene scene(Sphere(Vector3D(0,0,0),1));
-    std::cout<<scene.ToJsonObject().ToString()<<std::endl;
-    scene.Push(Sphere(Vector3D(1,1,1),2));
-    std::string tmp=scene.ToJsonObject().ToString();
-    std::cout<<scene.ToJsonObject().ToString()<<std::endl;
+    Scene scene(Sphere(Vector3D(0, 0, 0), 1));
+    std::cout << scene.ToJsonObject().ToString() << std::endl;
+    scene.Push(Sphere(Vector3D(1, 1, 1), 2));
+    std::string tmp = scene.ToJsonObject().ToString();
+    std::cout << scene.ToJsonObject().ToString() << std::endl;
     cjsonobj::CJsonObject jsonobj;
     jsonobj.Parse(tmp);
-    Scene scene_rev=SceneFromJsonObject(jsonobj);
-    std::cout<<scene_rev.ToJsonObject().ToString()<<std::endl;
+    Scene scene_rev = SceneFromJsonObject(jsonobj);
+    std::cout << scene_rev.ToJsonObject().ToString() << std::endl;
 }
 
 int main()
